c9322c252309d539e8a54a5eafeaef81d39fb0cb
rexfa <rex_fa@163.com>
2019-06-19 18:00:32 +0800

    继续研究

---
+++ components/ilist/ilist.js
@@ -0,0 +1,27 @@
+// components/itemlist/itemlist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  behaviors: [],
+  properties: {
+    myArray: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
